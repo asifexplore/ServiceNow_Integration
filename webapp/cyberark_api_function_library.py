5afbea0e336d8c3eb133249e42e26f22183033b6
MOHD ASIF <mohamedasif167@gmail.com>
2023-09-26 15:46:20 +0800

    Basic Requirements Completed
    Onboarding and Offboarding for individual accounts completed.

--- webapp/cyberark_api_function_library.py
+++ webapp/cyberark_api_function_library.py
@@ -1,6 +1,5 @@
 # This Library contains functions for various API calls
 import json
-
 import requests
 
 # server = 'https://dev108716.service-now.com/api/now/table/cmdb_ci_server'
@@ -11,31 +10,35 @@ import requests
 # Set the CyberArk credentials to use for authentication
 client_id = "servicenow.request@cyberark.cloud.9344"
 client_secret = "Cyberark1"
-
 identity_auth_endpoint = "https://aaw4349.id.cyberark.cloud/oauth2/platformtoken"
 base_url = "https://apjsesadesh.privilegecloud.cyberark.cloud/PasswordVault/"
 account_endpoint = "/API/Accounts"
 bulk_upload_endpoint = "/API/bulkactions/accounts/"
+# cyberark_array
+accountDetailsArray = []
 
-cyberark_array = []
+def clearAccountDetailsArray():
+    accountDetailsArray.clear()
 
+def setAccountDetailsArray():
+    accountDetailsArray = getAccountDetails()
+    # print(accountDetailsArray)
+
+def getAccountDetailsArray():
+    return accountDetailsArray
+
+# This function uses client_id, client_secret from the global variable set to obtain access token. This access token will be used during other API calls. s
 def authentication():
-
     # Set the grant type
     grant_type = "client_credentials"
-
     # Set the scope
     scope = "openid"
-
     # Set the headers
     headers_auth = {"Content-Type": "application/x-www-form-urlencoded"}
-
     # Set the data
     data = {"grant_type": grant_type, "client_id": client_id, "client_secret": client_secret, "scope": scope}
-
     # Send the POST request to obtain an access token
     response = requests.post(identity_auth_endpoint, headers=headers_auth, data=data, verify=False, timeout=5)
-
     # Parse the response JSON to obtain the access token
     response_json = json.loads(response.text)
     access_token = response_json['access_token']
@@ -53,10 +56,13 @@ def getAccountDetails():
     account_response = requests.get(base_url + account_endpoint, headers=api_headers, verify=False)
 
     comparison_data = json.loads(account_response.text)
+
     values = comparison_data.get("value", [])
-
+    newArr = [] 
     for value in values:
-
+        # print("value")
+        # print(value)
+        id = value.get("id")
         username = value.get("userName")
         safename = value.get("safeName")
         address = value.get("address")
@@ -64,20 +70,57 @@ def getAccountDetails():
         record_dict = {
             "username": username,
             "safename": safename,
-            "address": address
+            "address": address,
+            "id":id
         }
-        cyberark_array.append(record_dict)
+        newArr.append(record_dict)
 
-    print(cyberark_array)
+    return newArr
 
-    # Print the result array
-    for record in cyberark_array:
-        print("Username:", record["username"])
-        print("Safe Name:", record["safename"])
-        print("Address:", record["address"])
+# Function to check whether account is already inside PAM or not. | check_duplicate_Account, check_account_duplicate
+def check_account_duplicate(username, address, safe):
+    accountDetailsArray = getAccountDetails()
 
-    return cyberark_array
-def addAccount():
+    for record in accountDetailsArray:
+        if record["username"] == username and record["address"] == address and record["safename"] == safe:
+            # print("record.get('id')")
+            # print(record.get("id"))
+            # return record.get("id")
+            print("Error: Duplicate account found.")
+            return True
+
+    return False
+
+# Function to check whether account exists inside PAM regardless of any Safes 
+def check_account(address):
+    accountDetailsArray = getAccountDetails()
+    print(accountDetailsArray)
+    for accountDetailsRecord in accountDetailsArray:
+        # print(accountDetailsRecord["username"])
+        # print(username + " " + address)
+        # print(str(type(username)) + " " + str(type(address)))
+
+        # print(str(accountDetailsRecord["username"]) + " " + str(accountDetailsRecord["address"]))
+        # print(str(type(accountDetailsRecord["username"])) + " " + str(type(accountDetailsRecord["address"])))
+
+        # print(accountDetailsRecord["address"])
+        # if accountDetailsRecord["username"] == username and accountDetailsRecord["address"] == address:
+        if accountDetailsRecord["address"] == address:
+            print("Error: Duplicate account found.")
+            return True
+
+    return False
+
+def checkAccountForOffboarding(address):
+    accountDetailsArray = getAccountDetails()
+    for accountDetailsRecord in accountDetailsArray:
+        if accountDetailsRecord["address"] == address:
+            print("Error: Duplicate account found.")
+            return json.dumps({"status":True, "info":accountDetailsRecord})
+
+    return  json.dumps({"status":False, "info":None})
+
+def addAccount(AccountName,safeName,PlatformID,Address,UserName,Password):
     access_token = authentication()
 
     api_headers = {
@@ -86,17 +129,26 @@ def addAccount():
     }
 
     # Specify the account details to be uploaded
+    # account_data = {
+    #     "AccountName": "MyNewAccount",
+    #     "safeName": "TestSfe",
+    #     "PlatformID": "WinServerLocal",
+    #     "Address": "192.168.0.1",
+    #     "UserName": "admin",
+    #     "Password": "mypassword"
+    # } 
+
     account_data = {
-        "AccountName": "MyNewAccount",
-        "safeName": "TestSfe",
-        "PlatformID": "WinServerLocal",
-        "Address": "192.168.0.1",
-        "UserName": "admin",
-        "Password": "mypassword"
+        "AccountName": AccountName,
+        "safeName": safeName,
+        "PlatformID": PlatformID,
+        "Address": Address,
+        "UserName": UserName,
+        "Password": Password
     }
 
     # Check for duplicate account
-    if check_duplicate_Account(account_data["UserName"], account_data["Address"], account_data["safeName"]):
+    if check_account_duplicate(account_data["UserName"], account_data["Address"], account_data["safeName"]):
         print("Error: Duplicate account found. Exiting the program.")
         return
 
@@ -106,60 +158,53 @@ def addAccount():
     # Check the response status code
     if response.status_code == 201:
         print("Account uploaded successfully.", response.text)
+        return True
     else:
         print("Failed to upload account. Status code:", response.status_code)
         print("Error message:", response.text)
+        return False
 
-
-def bulkAccountUpload():
-
+def getAllPlatform():
     access_token = authentication()
 
     api_headers = {
         "Authorization": f"Bearer {access_token}",
         "Content-Type": "application/json"
     }
+    base_Curl = "https://apjsesadesh.privilegecloud.cyberark.cloud/PasswordVault"
+    get_platform = "/API/Platforms/"
+    platform_response = requests.get(base_Curl + get_platform, headers=api_headers, verify=False)
+    # print(platform_response)
+    dashboard_data = json.loads(platform_response.text)
+    # print(dashboard_data)
+    filtered_data = []
 
-
-    accounts_list = {
-            "accountsList": [
-                {
-                    "AccountName": "Administrator",
-                    "UserName": "JSmith",
-                    "SafeName": "TestSfe",
-                    "PlatformID": "WinServerLocal",
-                    "Address" : "10.10.10.9",
-                    "Password": "Cyberark1"
-                },
-                {
-                    "AccountName": "OracleAdmin",
-                    "UserName": "admin",
-                    "SafeName": "TestSfe",
-                    "PlatformID": "WinServerLocal",
-                    "Address": "192.168.53.12",
-                    "Password": "Cyberark1"
-                }
-            ]
+    for platform in dashboard_data['Platforms']:
+        filtered_platform = {
+            'id': platform['general']['id'],
+            'name': platform['general']['name'],
+            'systemType': platform['general']['systemType']
         }
+        filtered_data.append(filtered_platform)
+        return(filtered_data)   
 
-    # Checking for duplicate accounts before sending the POST request
-    for account in accounts_list['accountsList']:
-        if check_duplicate_Account(account['UserName'], account['Address'], account['SafeName']):
-            print(f"Error: Duplicate account found for {account['UserName']} at {account['Address']} in {account['SafeName']}. Stopping upload.")
-            return
+# def get_Safes(base_url, get_safe, access_token, platform):
+#     platform_id = platform['id']
+#     safes_data = get_safes(base_url, get_safe, access_token, platform_id)
 
-    # Step 4: Send the API request
-    response = requests.post(base_url+bulk_upload_endpoint, headers=api_headers, json=accounts_list, verify=False)
-    response.raise_for_status()
-
-    # Check the response status code
-    if response.status_code == 200:
-        print("Processing upload.", response.text)
-    else:
-        print("Failed to upload account. Status code:", response.status_code)
-        print("Error message:", response.text)
-
-
+#     if 'value' in safes_data:
+#         print(f"Safes for {platform['name']} (ID: {platform_id}):")
+#         for safe in safes_data['value']:
+#             print(f"- Safe Name: {safe}")
+#     else:
+#         error_code = safes_data.get('ErrorCode', '')
+#         error_message = safes_data.get('ErrorMessage', '')
+#         print(f"Error fetching safes for {platform['name']} (ID: {platform_id}):")
+#         print(f"Error Code: {error_code}")
+#         print(f"Error Message: {error_message}")
+    
+#     print("===================")
+	
 def removeAccount(username, address, safe):
     account_id = check_duplicate_Account(username, address, safe)
     if account_id is None:
@@ -190,8 +235,6 @@ def check_duplicate_Account(username, address, safe):
 
     for record in cyberark_array:
         if record["username"] == username and record["address"] == address and record["safename"] == safe:
-            return record.get("accountID")
-            print("Error: Duplicate account found.")
-            return True
+            return record.get("id")
 
     return False
--- webapp/views.py
+++ webapp/views.py
@@ -6,32 +6,115 @@ import sys
 import requests
 import urllib3
 import json
+from django.http import JsonResponse
 from requests import post
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-from webapp.api_function_library import getDB, getNetwork,getServer
+from webapp.api_function_library import *
+from webapp.cyberark_api_function_library import *
+# from webapp import config
 
-# Create your views here.
-def login(request):
-    return render(request,'webapp/login.html')
+configAccountDetailsArray = []
+configTotalArray = []
+configOnBoardingReturnArray = []
+testSaveArray = [] 
+
+offBoardingArray = []
 
 def home(request):
-    server = getServer()
-    db = getDB()
-    network = getNetwork()
-    totalArray = server + db + network
 
-    smallArray = []
-    for i in range(0,5):
-        smallArray.append(totalArray[i])
-    print(smallArray)
+    configTotalArray = getAllServiceNowData()
+    print("line 24")
+    print(configTotalArray)
+    print(configTotalArray[0]['name'])
+    configOnBoardingReturnArray.clear()
+    offBoardingArray.clear()
+    # Onboarding Logic
+    for item in configTotalArray:
+        if (check_account(item['address']) == False):
+            configOnBoardingReturnArray.append(item)
+
+        # Offboarding Logic 
+        data = checkAccountForOffboarding(item['address'])
+        results = json.loads(data)
+        print("results")
+        print(results)
+        print(type(results))
+        print(results['status'])
+        if (results['status'] == True):
+            item['acc_info'] = results['info']
+            print("item | Line 40")
+            print(item)
+            offBoardingArray.append(item)
+
+    # Call to get all platform  
+    allPlatforms = getAllPlatform()
+
+    # Store into dictionary 
+    thisDict = {}
+    for x in allPlatforms:
+        id = x['id']
+        thisDict[id] = "TestSfe"
+
+    # OffBoarding 
+
+    print("offBoardingArray")
+    print(offBoardingArray)
+    testSaveArray = configOnBoardingReturnArray
     context = {
-        'array':smallArray,
-        'totalCounttoOnboard':len(totalArray)
+        'onboardingArray':configOnBoardingReturnArray[:5],
+        'totalCounttoOnboard':len(configOnBoardingReturnArray),
+        'platformAndSafes':thisDict,
+        'offboardingArray':offBoardingArray[:5],
+        'totalCounttoOffboard':len(offBoardingArray)
     }
     return render(request,'webapp/home.html',context)
 
+# Works Fine
+def onboardAccFunction(request):
+    data = json.loads(request.body)
+    addAccount(data["accountName"],data["safeChosen"],data["platformChosen"],data["address"],data["username"],data["password"])
+    # print(getAccountDetails())
+    return JsonResponse('Success', safe=False)
+
+def offboardAccFunction(request):
+    data = json.loads(request.body)
+    print("data | Line 80")
+    print(data) 
+    removeAccount(data['objData']['acc_info']['username'], data['objData']['acc_info']['address'], data['objData']['acc_info']['safename'])
+    # addAccount(data["accountName"],data["safeChosen"],data["platformChosen"],data["address"],data["username"],data["password"])
+    # print(getAccountDetails())
+    return JsonResponse('Success', safe=False)
+
 def accounts(request):
+    print("testSaveArray")
+    print(testSaveArray)
+    configTotalArray = getAllServiceNowData()
+    configOnBoardingReturnArray.clear()
+    offBoardingArray.clear()
+    # Onboarding Logic
+    for item in configTotalArray:
+        if (check_account(item['address']) == False):
+            configOnBoardingReturnArray.append(item)
+
+    # Call to get all platform  
+    allPlatforms = getAllPlatform()
+
+    # Store into dictionary 
+    thisDict = {}
+    for x in allPlatforms:
+        id = x['id']
+        thisDict[id] = "TestSfe"
+    print("testSaveArray")
+    print(testSaveArray)
+    context = {
+        'array':configOnBoardingReturnArray, 
+        'platformAndSafes':thisDict,
+        "totalCounttoOnboard":len(configOnBoardingReturnArray)
+    }
+    return render(request,'webapp/accounts.html',context)
+
+def onboarding_account(request):
     server = getServer()
     db = getDB()
     network = getNetwork()
@@ -39,4 +122,36 @@ def accounts(request):
     context = {
         'array':totalArray
     }
-    return render(request,'webapp/accounts.html',context)
+    return render(request,'webapp/onboarding_accounts.html',context)
+
+def offboarding_account(request):
+    configTotalArray = getAllServiceNowData()
+    offBoardingArray.clear()
+    # Onboarding Logic
+    for item in configTotalArray:
+        # Offboarding Logic 
+        data = checkAccountForOffboarding(item['address'])
+        results = json.loads(data)
+        if (results['status'] == True):
+            item['acc_info'] = results['info']
+            offBoardingArray.append(item)
+
+    # Call to get all platform  
+    allPlatforms = getAllPlatform()
+
+    # Store into dictionary 
+    thisDict = {}
+    for x in allPlatforms:
+        id = x['id']
+        thisDict[id] = "TestSfe"
+
+    context = {
+        'array':offBoardingArray, 
+        'platformAndSafes':thisDict,
+        "totalCounttoOffboard":len(offBoardingArray)
+    }
+    return render(request,'webapp/offboarding_accounts.html',context)
+
+# Create your views here.
+def login(request):
+    return render(request,'webapp/login.html')
